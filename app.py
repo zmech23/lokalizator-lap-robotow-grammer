56abe927a86b800a335259f9f007e57732e682e3
zmech23 <zmech23@gmail.com>
2026-01-11 14:04:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import os
 
 app = Flask(__name__)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-EXCEL_PATH = os.path.join(BASE_DIR, "data.xlsx")
-
-df = pd.read_excel(EXCEL_PATH)
+df = pd.read_excel("data.xlsx")
+df.columns = df.columns.str.strip()  # usuwa spacje z nagłówków
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     wynik = None
-
     if request.method == "POST":
-        robot_id = request.form.get("robot_id")
-        wynik = df[df["ID"] == robot_id]
+        robot_id = request.form["robot_id"]
+
+        wynik = df[df["PROJEKT"] == robot_id]
 
         if wynik.empty:
-            wynik = "Nie znaleziono robota"
+            wynik = None
+
+    return render_template("index.html", wynik=wynik)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
     return render_template("index.html", wynik=wynik)
 
