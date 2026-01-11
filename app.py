e034ef25f228e222091b140ae4f3d92b49a7ff5c
zmech23 <zmech23@gmail.com>
2026-01-11 14:27:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,24 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# wczytanie danych
+# wczytanie Excela
 df = pd.read_excel("data.xlsx")
-df.columns = df.columns.str.strip()
+
+# normalizacja kolumny PROJEKT
+df["PROJEKT"] = df["PROJEKT"].astype(str).str.strip().str.upper()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     wynik = None
 
     if request.method == "POST":
-        robot_id = request.form["robot_id"]
-        wynik = df[df["PROJEKT"] == robot_id]
+        robot_id = request.form.get("robot_id")
 
-        if wynik.empty:
-            wynik = None
+        if robot_id:
+            robot_id = robot_id.strip().upper()
+            wynik = df[df["PROJEKT"] == robot_id]
 
     return render_template("index.html", wynik=wynik)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
