b3980d7836e08cf7c5d1a3daaa76800500e432a3
zmech23 <zmech23@gmail.com>
2026-01-11 13:51:19 +0100

    fix excel path

--- app.py
+++ app.py
@@ -5,25 +5,22 @@ import os
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-EXCEL_PATH = "data.xlsx"
+EXCEL_PATH = os.path.join(BASE_DIR, "data.xlsx")
+
 df = pd.read_excel(EXCEL_PATH)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     wynik = None
-    komunikat = None
 
     if request.method == "POST":
         robot_id = request.form.get("robot_id")
+        wynik = df[df["ID"] == robot_id]
 
-        robot = df[df["PROJEKT"] == robot_id]
+        if wynik.empty:
+            wynik = "Nie znaleziono robota"
 
-        if robot.empty:
-            komunikat = f"Nie znaleziono robota o ID: {robot_id}"
-        else:
-            wynik = robot.to_dict(orient="records")[0]
-
-    return render_template("index.html", wynik=wynik, komunikat=komunikat)
+    return render_template("index.html", wynik=wynik)
 
 if __name__ == "__main__":
     app.run()
