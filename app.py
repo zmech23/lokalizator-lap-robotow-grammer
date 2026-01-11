93ce6bbaf3899726cd8bcbd7156fbfe474064104
zmech23 <zmech23@gmail.com>
2026-01-11 02:04:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
+# Wczytanie Excela
+df = pd.read_excel("dane.xlsx")
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+    wynik = None
+    komunikat = None
+
     if request.method == "POST":
         robot_id = request.form.get("robot_id")
-        return f"Szukanie robota o ID: {robot_id}"
-    return render_template("index.html")
+
+        # SZUKANIE W KOLUMNIE PROJEKT
+        robot = df[df["PROJEKT"] == robot_id]
+
+        if robot.empty:
+            komunikat = f"Nie znaleziono robota o ID: {robot_id}"
+        else:
+            wynik = robot.to_dict(orient="records")[0]
+
+    return render_template("index.html", wynik=wynik, komunikat=komunikat)
 
 if __name__ == "__main__":
     app.run(debug=True)
