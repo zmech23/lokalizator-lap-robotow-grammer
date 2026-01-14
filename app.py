7f9226c8227c4680a75331013ba070fc04350f7d
zmech23 <zmech23@gmail.com>
2026-01-14 07:02:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-EXCEL_PATH = os.path.join(BASE_DIR, "data.xlsx")
+EXCEL_PATH = os.path.join(BASE_DIR, "data", "data.xlsx")
+
 
 df = pd.read_excel(EXCEL_PATH)
 df = df.fillna("")
