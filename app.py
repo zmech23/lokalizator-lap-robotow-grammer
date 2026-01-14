b6a7f1e06b445504a5d0d7ecd2e37b625f3c346c
zmech23 <zmech23@gmail.com>
2026-01-14 07:10:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 EXCEL_PATH = os.path.join(BASE_DIR, "data", "data.xlsx")
 
-
 df = pd.read_excel(EXCEL_PATH)
 df = df.fillna("")
 
