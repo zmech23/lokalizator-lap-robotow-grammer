9231e0effcef5b5f82d8ba857c28a250d0a38942
zmech23 <zmech23@gmail.com>
2026-01-16 17:40:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,43 @@
-const CACHE_NAME = "lokalizator-v1";
+const CACHE_NAME = "lap-robotow-v1";
 
 const FILES_TO_CACHE = [
   "/",
   "/static/style.css",
-  "/static/technik.png"
+  "/static/technik.png",
+  "/static/icon-192.png",
+  "/static/manifest.json"
 ];
 
-self.addEventListener("install", (event) => {
+// Instalacja
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
+// Aktywacja
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Obsługa requestów
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
